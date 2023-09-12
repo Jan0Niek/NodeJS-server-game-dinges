2961aa8f9ec8daaa3a0a39b153f015f85e5fdede
Jan0Niek <yannick.eleveld@coderclass.nl>
2023-09-12 14:54:16 +0200

    server is nu

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const http = require('http');
 const fs = require('fs');
 
+const port = 3000;
+const host = '127.0.0.1'
+
+const server = http.createServer((request, response) => {
+    response.statusCode = 200;
+    response.setHeader('Content-type', 'text/html');
+    response.end('hier staat tekst?');
+});
+
+server.listen(port, host, () => {
+    console.log('server started enzo')
+});
