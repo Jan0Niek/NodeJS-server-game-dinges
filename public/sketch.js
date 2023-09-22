86c6852e717ba1d408583cdfc0ac5032e170a291
Jan0Niek <yannick.eleveld@coderclass.nl>
2023-09-22 13:39:06 +0200

    EINDELIJK EEN BEETJE GOEDE PLAYER CONTROLS EN COLLISIONS MET BLOCKS
    het is zover, je kan platformen in de platformer!!!!!

--- public/sketch.js
+++ public/sketch.js
@@ -1,25 +1,28 @@
 let socket;
 
 let gravity = 12;
-let blocks;
+let blocks=[];
 let player;
 let block1;
 
 function setup() {
-  createCanvas(800, 700);
+  createCanvas(windowWidth, windowHeight);
   socket = io();  //dit fixt het met dat alleen localhost werkt, nu kan ook 127.0.0.1
   
   block1 = new Block(20, 360, 220, 40, [0, 255, 0]);
-  player = new Player(50, 120, 12);
+  block2 = new Block(200, 300, 40, 40, [20, 200, 20]);
+  blocks.push(block1, block2);
+  player = new Player(50, 120, 2);
 
 }
 
 function draw() {
   background(255);
-  player.move(key, 2);
-  player.collision(block1);
+  
+  player.platformGravityJumpHeightSideMovementSpeedFriction(1, 22, 2, 0.8, blocks);
   player.draw();
-
-  block1.draw();
+  blocks.forEach(block => {
+    block.draw();
+  });
 
 }
