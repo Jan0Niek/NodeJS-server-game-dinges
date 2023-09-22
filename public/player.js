1758a80af6e5bdb03470fa2109ab0b52c7b2f8a0
Jan0Niek <yannick.eleveld@coderclass.nl>
2023-09-22 13:45:47 +0200

    kleine verandering, opschonen-esque

--- public/player.js
+++ public/player.js
@@ -8,46 +8,6 @@ class Player{
         this.velX=0.1;
         this.velY=0.1;
     }
-    // move(key, gravity){
-    //     //wasd
-    //     if (keyIsDown(65)){
-    //         // this.velX -= this.accel
-    //         this.x -= this.accel;
-    //     }
-    //     if (keyIsDown(68)){
-    //         // this.velX += this.accel;
-    //         this.x += this.accel;
-    //     }
-    //     //jump etc moet nog
-
-    //     //gravity
-    //     // this.velY += gravity;
-
-    //     // this.x += this.velX;
-    //     this.y += gravity;
-    //     // this.velX /= 2;
-        
-    // }
-    // //wordt in de main loop ge- for-looped door de lijst van alle blokken
-    // collision(block){
-    //     let testX = this.x;
-    //     let testY = this.y + gravity;
-    //     if (this.x + this.width >= block.x && this.x <= block.x + block.width && this.y + this.height >= block.y && this.y <= block.y + block.height) {  
-    //         this.y -= gravity-gravity;
-    //     }
-    //     testX = this.x + this.accel;
-    //     testY = this.y;
-    //     if (this.x + this.width >= block.x && this.x <= block.x + block.width && this.y + this.height >= block.y && this.y <= block.y + block.height) {    
-    //         this.x -= this.accel;
-    //     }
-    //     testX = this.x - this.accel;
-    //     testY = this.y;
-    //     if (this.x + this.width >= block.x && this.x <= block.x + block.width && this.y + this.height >= block.y && this.y <= block.y + block.height) {    
-    //         this.x += this.accel;
-    //     }
-    //     //zorg dat hij geen snelheid opbouwt als hij op een blok staat CHECK
-        
-    // }
     platformGravityJumpHeightSideMovementSpeedFriction(
         gravity,
         jumpHeight,
@@ -57,13 +17,14 @@ class Player{
       ) {
         this.velY += Number(gravity);
         this.y += Number(this.velY);
+        //loop door alle blocks zodat je er niet doorheen valt 
         blocks.forEach(block => {
           if (this.x + this.width >= block.x && this.x <= block.x + block.width && this.y + this.height >= block.y && this.y <= block.y + block.height) {
             while ((this.x + this.width >= block.x && this.x <= block.x + block.width && this.y + this.height >= block.y && this.y <= block.y + block.height)) {
               this.y +=(Math.abs(Number(this.velY)) /Number(this.velY)) *-1;
             }
             this.velY=0;
-            
+            //moet nog fixen dat je meer spring als je langer ingedrukt houdt
             if (keyIsDown(32) || keyIsDown(87)){
               this.velY = -jumpHeight;
             }
