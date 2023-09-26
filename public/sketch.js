e7f4feec36143e0cc69e0d5b0b622a8a92284cad
potvisguppie <72612877+potvisguppie@users.noreply.github.com>
2023-09-26 14:40:56 +0200

    Update sketch.js
    alles dat in yannick zijn branch is nu ook in die van mij.

--- public/sketch.js
+++ public/sketch.js
@@ -10,7 +10,34 @@ function setup() {
 }
 
 function draw() {
+<<<<<<< Updated upstream
   line(10, 20, 180, 160);
+=======
+  background(255);
+  text(frameRate(), 20, 20);
+  blocks.forEach(block => {
+    velX = block.move(2);
+    block.draw();
+  });
+  //movement done
+  player.platformGravityJumpHeightSideMovementSpeedFriction(0.9, 24, 2, 0.8, blocks, velX);
+  player.draw();
+
+  fill(186, 41, 11)
+  ellipse(200, 260 , 170, 140);
+  fill(186, 41, 11)
+  ellipse(200, 200, 85, 110)
+  fill(15, 35, 135)
+  ellipse(250, 340, 40, 80)
+  fill(15, 35, 135)
+  ellipse(150, 340, 40, 80)
+  fill(255,255,255)
+  rect(172, 205, 60, 20, 20)
+  rect(205, 180, 20, 20, 10)
+  rect(175, 180, 20, 20, 10)
+
+  fill(186, 41, 11)
+>>>>>>> Stashed changes
   
   // color()
 }
