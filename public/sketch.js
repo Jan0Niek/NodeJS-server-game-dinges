ec5dfd02c2df73d3d1a66d4b7a445d4d4d30498e
potvisguppie <72612877+potvisguppie@users.noreply.github.com>
2023-09-26 14:48:30 +0200

    Update sketch.js
    alles dat in yannick zijn branch is nu ook in die van mij. 2

--- public/sketch.js
+++ public/sketch.js
@@ -19,18 +19,14 @@ function setup() {
 }
 
 function draw() {
-  line(10, 20, 180, 160);
+  background(255);
+  text(frameRate(), 20, 20);
+  blocks.forEach(block => {
+    block.move(2);
+    block.draw();
+  });
+  //movement done
+  player.platformGravityJumpHeightSideMovementSpeedFriction(0.9, 24, 2, 0.8, blocks);
+  player.draw();
   
-  // color()
-}
-
-
-function keyPressed(){
-  const data = {
-    x: mouseX, 
-    y: mouseY,
-    key: key
-  }
-  socket.emit('typed', data);
-  text(data.key, data.x, data.y);
 }
