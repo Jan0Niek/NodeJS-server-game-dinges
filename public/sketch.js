0e4adaa7a7be3253f22964d6f5d55ede48764394
Jan0Niek <yannick.eleveld@coderclass.nl>
2023-10-03 14:33:34 +0200

    verwijderd troep

--- public/sketch.js
+++ public/sketch.js
@@ -8,21 +8,12 @@ let block1;
 function setup() {
   createCanvas(windowWidth, windowHeight);
   socket = io();  //dit fixt het met dat alleen localhost werkt, nu kan ook 127.0.0.1
-  
-  block1 = new Block(20, 360, 220, 40, [0, 255, 0]);
-  block2 = new Block(200, 300, 40, 40, [20, 200, 20]);
-  blocks.push(block1, block2);
-  player = new Player(50, 120, 2);
 
 }
 
 function draw() {
   background(255);
   
-  player.platformGravityJumpHeightSideMovementSpeedFriction(1, 22, 2, 0.8, blocks);
-  player.draw();
-  blocks.forEach(block => {
-    block.draw();
-  });
+  circle(mouseX, mouseY, 20);
 
 }
