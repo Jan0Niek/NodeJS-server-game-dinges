02c87a0861cf96cc20d8b776ad36202138464377
Jan_Niek <yannick.eleveld@coderclass.nl>
2024-03-20 00:50:45 +0100

    ts

--- public/sketch.js
+++ public/sketch.js
@@ -1,7 +1,9 @@
 let thing;
+let floor; //typescript?
 
 function setup() {
 	new Canvas(500, 500);
+	world.gravity.y = 9.81;
 
 	class Player extends Sprite{
 		constructor(x, y, width, height){
@@ -15,11 +17,13 @@ function setup() {
 		}
 	}
 	thing = new Player(70, 70, 20, 40);
-	thing.applyForce(30, 40)
+	new Sprite(130, 150, 20, 20)
+	floor = new Sprite(250, height, width, 14, 'k')
 }
 
 function draw() {
 	background('gray');
+	floor.y--;
 	
 	
 }
