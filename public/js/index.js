711b54a47e46cf190a1fb4cf3b09851796da1f23
Jan_Niek <yannick.eleveld@coderclass.nl>
2024-03-24 00:45:27 +0100

    klein commitje

--- public/js/index.js
+++ public/js/index.js
@@ -7,13 +7,16 @@ let Block = declareBlock();
 new Canvas(900, 500)
 world.gravity.y = 9.81;
 
-new Sprite(50, 50, 50, 50);
+let sprit = new Sprite(50, 50, 50, 50);
+sprit.friction = 100;
 
 new Sprite (width/2, height-9, width, 20, 'k')
-let ablock = new Block(10, 10, 100, 40, 5);
+let ablock = new Block(10, 10, 100, 40, 3);
 
 //main game loop enz
 function draw(){
     background(0, 123, 123);
     ablock.control()
+    sprit.rotation = 0;
+
 }
