e7a06298d9cd7dec0d1a42f52cd782bf98cd7427
Jan_Niek <yannick.eleveld@coderclass.nl>
2024-03-25 10:44:04 +0100

    uh-huh

--- public/js/index.js
+++ public/js/index.js
@@ -6,9 +6,14 @@ let Block = declareBlock();
 //hier de overige set-up and such
 new Canvas(900, 500)
 world.gravity.y = 9.81;
+world.allowSleeping = false;
+
 
 let sprit = new Sprite(50, 50, 50, 50);
-sprit.friction = 100;
+for (let i = 0; i < 5; i++) {
+    new Sprite(random(20, 900), random(20, 480), random(4, 60), random(4, 60))
+}
+sprit.friction = 0.1;
 
 new Sprite (width/2, height-9, width, 20, 'k')
 let ablock = new Block(10, 10, 100, 40, 3);
@@ -16,6 +21,9 @@ let ablock = new Block(10, 10, 100, 40, 3);
 //main game loop enz
 function draw(){
     background(0, 123, 123);
+    allSprites.draw()
+    text(frameCount, 20, 20)
+    //uhuh
     ablock.control()
     sprit.rotation = 0;
 
