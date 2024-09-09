f78dbcc971c1f4a7019793763b8aa89c58fc063e
Jan_Niek <yannick.eleveld@coderclass.nl>
2024-09-09 22:51:53 +0200

    lobby-playerkeuze-scherm is perfect

--- public/js/sceneManager.js
+++ public/js/sceneManager.js
@@ -16,14 +16,12 @@ const scenes =
             socket.emit("readyUp", (readiness));
         }
 
-        let player1Exists;
-        let player2Exists;
 
         textStyle(BOLD);
         strokeWeight(1);    
 
-        let button1 = new Button(200, 400, 300, 100, color(200, 80, 160), 'P1', 20, requestToBePlayerX, (1), true, undefined, "P1: 🫵");
-        let button2 = new Button(600, 400, 300, 100, color(200, 80, 160), 'P2', 20, requestToBePlayerX, (2), true, undefined, "P2: 🫵");
+        let button1 = new Button(200, 400, 300, 100, color(200, 80, 160), 'P1', 20, requestToBePlayerX, (1));
+        let button2 = new Button(600, 400, 300, 100, color(200, 80, 160), 'P2', 20, requestToBePlayerX, (2));
 
         let readyButton = new Button(canvas.hw, 680, 300, 40, color(20, 240, 20), "Ready up", 40, readyUp, null, true, color(0, 60, 0), "Readied", color(0, 0, 0), color(240, 240, 240));
         readyButton.autoDraw = false;
@@ -46,34 +44,27 @@ const scenes =
             text("my name is: " + chosenUsername, canvas.hw, 40);
             text("i'm player " + myPlayerNum, canvas.hw, 50);
             text("OTHER players in lobby: ", 40, textSize());
+            
+            button1.text = button1.origText;
+            button2.text = button2.origText;
+            
             let i=0;
             otherPlayers.forEach((otherP) => {
                 text(otherP.username, 40, 24+i*textSize());
                 i++;
                 if(otherP.playerNum == 1) {
                     button1.text = `${button1.origText}: ${otherP.username}`;
-                    player1Exists = true;
                 }
                 if(otherP.playerNum == 2) {
                     button2.text = `${button2.origText}: ${otherP.username}`;
-                    player2Exists = true;
                 }
             });
 
-            if(!player1Exists)button1.text = button1.origText;
-            if(!player2Exists)button2.text = button2.origText;
-            if(myPlayerNum != -1){
-                if(myPlayerNum == 1){
-                    button1.img = emojiFinger;
-                } else{
-                    button2.img = emojiFinger;
-                }
+            if(myPlayerNum == 1){
+                button1.text = `${button1.origText}: 🫵 (you!)`;
+            }else if(myPlayerNum == 2){
+                button2.text = `${button2.origText}: 🫵 (you!)`;
             }
-            player1Exists = false;
-            player2Exists = false;
-            // button1.image = undefined;
-            // button2.image = undefined;
-
 
         }
     }
