c5b040a3ac4bf48afa430a89f73a3e149ee4bd97
Jan0Niek <73346551+Jan0Niek@users.noreply.github.com>
2025-01-28 00:55:18 +0100

    'this' works (remove other foreach!)

--- public/js/localPlay/selectables.js
+++ public/js/localPlay/selectables.js
@@ -92,13 +92,15 @@ function declareSelectables(TILESIZE){
     selectables.h = TILESIZE.y
     selectables.color = color(100, 200, 100)
 
-    selectables.update = function(){
+    selectables.checkSelection = function(){
         selectables.forEach(selectable =>{
             if (selectable.mouse.presses()) {
-                console.log("selectable was pressed!");
+                // console.log("selectable was pressed!" + selectable.idNum);
                 
                 selectable.color = random(0, 255)
-                selectables.selected = false
+                selectables.forEach(selectable => {
+                    selectable.selected = false
+                });
                 selectable.selected = true
             }
         })
@@ -110,6 +112,7 @@ function declareSelectables(TILESIZE){
         if(kb.pressing('arrowDown'))  selectables.verticalInput += 1
         if(kb.pressing('arrowUp'))    selectables.verticalInput -= 1
     }
+    selectables.update = function(){ selectables.checkSelection() }
 
 
 
@@ -128,16 +131,18 @@ function declareSelectables(TILESIZE){
     }
     blocks.movement = function(){
         this.velocity.set(0, 0)
-        
         this.vel.x += selectables.horizontalInput*this.movementspeed
         this.vel.y += selectables.verticalInput*this.movementspeed
     }
     blocks.update = function(){
+        selectables.checkSelection()
+        //how 'this' works in js is a mess... hate it
         if (this.selected) {
             this.collider = 'd'
             this.stopGlideMovement()
             this.movement()
         } else {
+            this.vel.set(0, 0) // have seriously no clue why i have to set their velocity to nil, they should be of collider kinematic and thusly not move...
             this.collider = 'k'
         }
     }
@@ -145,6 +150,7 @@ function declareSelectables(TILESIZE){
     
     let selectableEnemies = new selectables.Group()
     selectableEnemies.shape = 'triangle';
+    selectableEnemies.movementspeed = 3
 
     selectableEnemies.collides(allSprites, killCheck) //kan ook in 1x enkel collision checken met player-(group?)
     
@@ -158,16 +164,16 @@ function declareSelectables(TILESIZE){
         //zorg dat--e heen en weer beweegt, mogelijk met 2 ge-GlueJointe sensoren
     }
     selectableEnemies.controlledMovement = function(){
-        this.velocity.set(0, 0)
+        selectableEnemies.velocity.set(0, 0)
         
-        this.vel.x += selectables.horizontalInput*this.movementspeed
+        selectableEnemies.vel.x += selectables.horizontalInput*selectableEnemies.movementspeed
     }
 
     selectableEnemies.update = function(){
-        if (this.selected) {
-            this.controlledMovement()
+        if (selectableEnemies.selected) {
+            selectableEnemies.controlledMovement()
         } else {
-            this.normalMovement()
+            selectableEnemies.normalMovement()
         }
     }
 }
